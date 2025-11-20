3553001eb39afbf07b1206b0acb5c7f3d756d0bc
Habibur Rahman <mhr.habibur@gmail.com>
2025-11-20 19:59:21 +0600

    Added data type.

---
+++ data-type.js
@@ -0,0 +1,55 @@
+// data type
+
+// string
+
+let userName = 'john doe';
+console.log(userName);
+
+
+// to check data type
+
+let checkDataType = typeof userName
+console.log(checkDataType);
+
+
+// number
+
+let score = 478;
+console.log(score);
+
+// string interpolation
+console.log(`My score is ${score}`);
+
+
+// boolean
+let isActive = true;
+console.log(isActive);
+
+// undefined
+
+let address = undefined;
+console.log(address);
+
+// null
+
+let height = null;
+console.log(height);
+
+
+// big init
+let distance = 123456789123456789123456789;
+console.log(distance);
+
+
+// object
+let person = { name: 'john doe', age: 25 };
+console.log(person.name);
+console.log(person.age);
+console.log(person['name']);
+console.log(person['age']);
+
+
+// symbol
+
+let mySymbol = Symbol(123);
+console.log(mySymbol);
