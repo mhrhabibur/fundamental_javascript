7c3febe03465152f952a0b5a0bfec88df5ab5913
Habibur Rahman <mhr.habibur@gmail.com>
2025-11-20 19:45:29 +0600

    initial commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+// Variables
+
+var name = 'jenny';
+console.log(name);
+
+let userName = 'john';
+console.log(userName);
+
+let myNumber = 12345;
+console.log(myNumber);
+
+
+// Constant
+
+const address = 'dhaka';
+console.log(address);
+
