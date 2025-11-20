3bd85aa6dc68e86ef301ff7e7d11c06417ce7e15
Habibur Rahman <mhr.habibur@gmail.com>
2025-11-20 20:25:14 +0600

    Added operators.

---
+++ operators.js
@@ -0,0 +1,57 @@
+// arithmetic operator
+
+
+// addition
+
+let x = 10;
+let y = 20;
+let sum = x + y;
+console.log(sum);
+
+// subtraction
+let income = 200;
+let expense = 160;
+let savings = income - expense;
+console.log(savings);
+
+// multiplication
+let wagesPerHour = 45;
+let totalHour = 40;
+let totalWages = wagesPerHour * totalHour;
+console.log(totalWages);
+
+// division
+let totalBalls = 160;
+let totalPerson = 40;
+let ballPerPerson = totalBalls / totalPerson;
+console.log(ballPerPerson);
+
+
+// modulus (Reminder after division)
+let p = 12;
+let s = 5;
+let reminder = p % s;
+console.log(reminder);
+
+// increment
+let num = 12;
+num += 1; // num = num + 1;
+console.log(num);
+
+// decrement
+let myNumber = 100;
+myNumber -= 10; // myNumber = myNumber - 10;
+console.log(myNumber);
+
+// exponentiation
+let e = 10;
+let result = 2 ** 2;
+console.log(result);
+
+
+// comparison (it returns boolean)
+let a = 10;
+let b = 20;
+let isGreater = a > b;
+console.log(isGreater);
+
